061aad3b76a0ae0e2e905ed4a447c7e0b50cad2e
MANE MORO <3520543@ppti-24-302-03.ufr-info-p6.jussieu.fr>
2017-02-03 12:36:59 +0100

    Ok

--- __init__.py
+++ __init__.py
@@ -10,6 +10,6 @@ from OrNoir import Attaque2Strategie,DefenseStrategie
 
 team1 = SoccerTeam(name="team1",login="etu1")
 team2 = SoccerTeam(name="team2",login="etu2")
-team1.add("Cavani",Attaque2Strategie()) #Strategie qui ne fait rien
-team2.add("Paul",DefenseStrategie())
-team2.add("Cavani",Attaque2Strategie()) 
+team1.add("Rocket",Attaque2Strategie()) #Strategie qui ne fait rien
+team2.add("Micro-Ice",DefenseStrategie())
+team2.add("D-Joke",Attaque2Strategie()) 
